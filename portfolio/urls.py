0f3616b71cf7ef49cd6fdc223466cd2aaa6efa01
Shivanshu <shivanshuojha890@gmail.com>
2021-12-06 18:48:37 +0530

    Initial Commit

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -9,6 +9,8 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile/',include('file.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:
