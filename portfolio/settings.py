4641f046b453a40f4a8b32c502430d03d0b75a11
Shivanshu <shivanshuojha890@gmail.com>
2021-12-06 18:49:37 +0530

    Procfil commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s%p@ey(9-)e%r@#bbhe+#p#nw_4hn--z2=9_z@#*^u=60ndiez'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','sjprofile.herokuapp.com']
 
